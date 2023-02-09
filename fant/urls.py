240ecf926cd40a16b3f2b242a04ec1d31954f707
Viktor Tingstad <viktorti@stud.ntnu.no>
2023-02-10 00:09:41 +0100

    Primært estetiske endringer - Gått over linker og pekt til rett side. - Gitt css-stil til komponenter tilknyttet visning av annonser - Oppdatert database-modeller til å reflektere planlagt db-struktur
    Issue: #17
    Utviklingsoppgaver: #18, #19

--- fant/urls.py
+++ fant/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('page1/', views.page1, name='page1'),
     path('posts/', include('posts.urls'), name='posts'),
     path('post/', include('posts.urls'), name='post_detail'),
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ static/js/main.js
@@ -0,0 +1,48 @@
+// Clickable posts
+$(".single_post").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+});
+
+// Dark Mode
+let darkMode = false;
+
+const lightcolors = {
+    '--primary-color': '#ffcd80',
+    '--secondary-color': '#ff9900',
+    '--tertiery-color': '#efefef',
+    '--box-color1': '#eee',
+    '--box-color2': '#ddd',
+    '--text-color1': '#fff',
+    '--text-color2': '#000'
+};
+
+const darkcolors = {
+    '--primary-color': '#ff9900',
+    '--secondary-color': '#ffcd80',
+    '--tertiery-color': '#1a1a1a',
+    '--box-color1': '#111',
+    '--box-color2': '#222',
+    '--text-color1': '#000',
+    '--text-color2': '#fff'
+};
+
+function toggleDarkMode() {
+    if (darkMode) {
+        for (let color in lightcolors) {
+            document.documentElement.style.setProperty(color, lightcolors[color]);
+        }
+    } else {
+        for (let color in darkcolors) {
+            document.documentElement.style.setProperty(color, darkcolors[color]);
+        }
+    }
+    darkMode = !darkMode;
+}
+
+document.addEventListener("keypress", event => {
+    if (event.key == "d") {
+        toggleDarkMode();
+    }
+});
+// End Dark Mode
