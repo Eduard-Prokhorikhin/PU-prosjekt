0583301dd52d9d4417d84514c515de7ce0fa9f37
Viktor Tingstad <viktorti@stud.ntnu.no>
2023-02-13 23:52:48 +0100

    Added some functionality for renting products
    Issue: #24

--- fant/views.py
+++ fant/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from posts.models import Post
 
-# Create your views here.
 def index(request):
     return render(request, 'page.html')
+
+def rent_product(request, pk):
+    Post.objects.filter(pk=pk).update(status='UNAVAILABLE')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def reset_all_availability(request):
+    Post.objects.all().update(status='AVAILABLE')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
