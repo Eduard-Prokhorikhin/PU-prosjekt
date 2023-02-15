f4d37bcc8b32fec53f9506675913b68178273c53
Ragnar Dreier <ragnad@stud.ntnu.no>
2023-02-15 15:47:51 +0100

    #7 created filter button and start dropdown

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,9 @@ $(".single_post").click(function() {
     return false;
 });
 
+// Return to home page
+
+
 // Dark Mode
 let darkMode = false;
 
@@ -46,3 +49,26 @@ document.addEventListener("keypress", event => {
     }
 });
 // End Dark Mode
+
+
+
+// Filter dropdown
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownFilter() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
