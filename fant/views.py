b8dec3de723685eb03dec6642ba43c5ac3ada995
Viktor Tingstad <viktorti@stud.ntnu.no>
2023-02-23 09:55:57 +0100

    Minor UI changes to profile page, in light of rental-implementation
    Issue: #5 and #29

--- fant/views.py
+++ fant/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.contrib import messages
 from posts.models import *
 
 def index(request):
@@ -10,16 +11,17 @@ def index(request):
 
 @login_required
 def rent_product(request, pk):
+    url= reverse('post_detail', args=[str(pk)])
     post = Post.objects.get(pk=pk)
     if post.author==request.user:
-        print("test")
-        url= reverse('post_detail', args=[str(pk)])
-        return redirect(url)
+        messages.success(request, 'Du kan ikke leie noe du selv eier...')
+    elif post.status=='UNAVAILABLE':
+        messages.success(request, 'Dette produktet er allerede leid ut...')
     else:
         Post.objects.filter(pk=pk).update(status='UNAVAILABLE')
         Rental.objects.create(
             post=post,
             renter=User.objects.get(pk=request.user.id),
             )
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return redirect(url)
 
