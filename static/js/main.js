ce38e5166c09b805238e27e723301cc47c36b243
Viktor Tingstad <viktorti@stud.ntnu.no>
2023-02-23 12:57:17 +0100

    Bug fix for links inside clickable divs
    Fixed so that inputfields and links may be inside a div with 'onclick'-attribute

--- static/js/main.js
+++ static/js/main.js
@@ -87,18 +87,21 @@ document.getElementById("headerInputField").addEventListener("keypress", event =
 toggle between hiding and showing the dropdown content */
 function dropdownFilter() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
+
+// Makes it possible to prevent default on links etc.
+document.addEventListener("click", {}, true);
