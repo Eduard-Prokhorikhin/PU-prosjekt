4de9e1170e36e0a2a533fcdc1a0d4fceb58c5262
Aiman <aimann@stud.ntnu.no>
2023-03-07 13:34:56 +0100

    Fixed last see userprofile-changes

--- posts/views.py
+++ posts/views.py
@@ -148,7 +148,7 @@ def getRentedDays(post):
 def renter_detail(request, pk):
     user = User.objects.get(pk=pk)
     next = request.META.get('HTTP_REFERER')
-    user_posts = Post.objects.filter(author=request.user, status='AVAILABLE').order_by('-pub_date')
+    user_posts = Post.objects.filter(author=user, status='AVAILABLE').order_by('-pub_date')
     
     context = {
         'user': user,
