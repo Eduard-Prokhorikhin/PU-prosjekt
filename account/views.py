be57c417dc5a0d8d1444b00874d53ac45959486f
Eduard Prokhorikhin <eduardpr@stud.ntnu.no>
2023-03-21 14:11:18 +0100

    Finished combining endRental with review

--- account/views.py
+++ account/views.py
@@ -11,9 +11,9 @@ from posts.models import *
 def profilePage(request):
     context = {
         'rent_requests' : RentRequest.objects.filter(post__in=Post.objects.filter(author=request.user), status= 'PENDING').order_by('-end_date'),
-        'rentals': RentRequest.objects.filter(renter=request.user, end_date__gte=datetime.now().date()).exclude(status='REJECTED').order_by('-start_date'),
+        'rentals': RentRequest.objects.filter(renter=request.user, review=False).exclude(status='REJECTED').order_by('-start_date'),
         'posts': Post.objects.filter(author=request.user).order_by('-pub_date'),
-        'history': RentRequest.objects.filter(renter=request.user, status='ACCEPTED', end_date__lt=datetime.now().date()).order_by('-end_date'),
+        'history': RentRequest.objects.filter(renter=request.user, status='ACCEPTED', review=True).order_by('-end_date'),
     }
     return render(request, 'profile.html', context)
 
@@ -63,12 +63,12 @@ def logoutPage(request):
     return redirect('login')
 
 def endRental(request, pk):
-    rental= RentRequest.objects.filter(pk=pk)
+    rental= RentRequest.objects.get(pk=pk)
     if rental.start_date > datetime.now().date():
         messages.error(request, "Kan ikke avslutte leie før startdato av forholdet")
         return redirect('index')
     else:
-        rental.update(end_date= datetime.now().date())
+        RentRequest.objects.filter(pk=pk).update(end_date= datetime.now().date())
         return redirect('rate_rental', pk=pk)
 
 def acceptRental(request, pk):
