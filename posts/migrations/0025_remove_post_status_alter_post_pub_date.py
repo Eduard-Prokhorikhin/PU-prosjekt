e2f6ba640b007a4c7bffcf5197fabdc451c85bde
Viktor Tingstad <viktorti@stud.ntnu.no>
2023-03-23 09:55:38 +0100

    Moved profile "Add new post"-button to a tile

--- posts/migrations/0025_remove_post_status_alter_post_pub_date.py
+++ posts/migrations/0025_remove_post_status_alter_post_pub_date.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='post',
-            name='status',
-        ),
         migrations.AlterField(
             model_name='post',
             name='pub_date',
